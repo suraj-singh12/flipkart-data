45b2020a457a5e0a99c030eaffded2d6dccb6427
suraj-singh12 <surajsingh5092@gmail.com>
2022-07-26 00:56:26 +0530

    update patch

--- uploadImgs.js
+++ uploadImgs.js
@@ -4,28 +4,28 @@ let path = require('path');
 
 
 uploadImgs = (dirPath) => {
-
     let files = fs.readdirSync(dirPath);
-
 
     for (file of files) {       //  for each file
         if (!file.includes('json')) continue;
-        console.log(file);
+
+        console.log(dirPath + '/' + file);
         let data = require(dirPath + '/' + file);
 
         for (obj of data) {
-            console.log(obj['item_id'], ' ', obj['image']);
+            // console.log(obj['item_id'], ' ', obj['image']);
 
             // call api and upload image
             imgbbUploader("f4a18fded7b0472b695f71cbf6854d1f", obj['image'])
                 .then((response) => {
-                    console.log(response);
-                    console.log(response.image.url);
+                    // console.log(response);
+                    // console.log(response.image.url);
 
                     // set new image url
                     obj['image'] = response.image.url;
                 })
                 .catch((error) => console.error(error));
+                break; // remove it
         }
         // update this file with new img urls
         fs.unlink(dirPath + '/' + file, (err) => {
@@ -36,7 +36,6 @@ uploadImgs = (dirPath) => {
             if(err) throw err;
             console.log('updated ' + dirPath + '/' + file + ' written');
         });
-        break;
     }
 }
 
