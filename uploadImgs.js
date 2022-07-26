8376ed65d6e2eb371ea44f3bcdd3d1e184499a67
suraj-singh12 <surajsingh5092@gmail.com>
2022-07-26 14:30:17 +0530

    update patch (working version of uploadImgsToImgBB)

--- uploadImgs.js
+++ uploadImgs.js
@@ -2,23 +2,22 @@ const imgbbUploader = require("imgbb-uploader");
 let fs = require('fs');
 let path = require('path');
 
-
+// upload all images of a file
 const uploadImgsOf = async (file, dirPath) => {
     let fileData = require(dirPath + '/' + file);
     let i = 1;
     for (obj of fileData) {
         await imgbbUploader("f4a18fded7b0472b695f71cbf6854d1f", obj['image'])
-        .then((response) => {
-            console.log(response);
-            obj['image'] = response.image.url;
-        })
-        .catch((error) => console.error(error));
-        i++;
-        if(i == 10) break;
+            .then((response) => {
+                console.log(response);
+                obj['image'] = response.image.url;
+            })
+            .catch((error) => console.error(error));
     }
     return fileData;
 }
 
+// upload images of all files in given directory & update it then
 const uploadImgsOfDir = (dirPath) => {
     let files = fs.readdirSync(dirPath);
 
@@ -29,54 +28,23 @@ const uploadImgsOfDir = (dirPath) => {
         console.log(dirPath + '/' + file);
 
         uploadImgsOf(file, dirPath)
-        .then((newData) => {
-            console.log(newData);
-            console.log('back to caller');
-        })
+            .then((newData) => {
+                // console.log(newData);
+                console.log('back to caller/ new data received, now writing');
+
+                // update this file with new img urls
+                fs.unlink(dirPath + '/' + file, (err) => {
+                    if (err) throw err;
+                    console.log(dirPath + '/' + file + ' deleted');
+                })
+                fs.writeFile(dirPath + '/' + file, JSON.stringify(data), { flag: 'w' }, (err) => {
+                    if (err) throw err;
+                    console.log('updated ' + dirPath + '/' + file + ' written');
+                });
+            })
         break;
     }
 }
 
+
 uploadImgsOfDir('./res/json0');
-
-// uploadImgs = (dirPath) => {
-//     let files = fs.readdirSync(dirPath);
-
-//     for (file of files) {       //  for each file
-//         if (!file.includes('json')) continue;
-//         if (file.includes('structure.json')) continue;
-
-//         console.log(dirPath + '/' + file);
-//         let data = require(dirPath + '/' + file);
-
-//         for (obj of data) {
-//             // console.log(obj['item_id'], ' ', obj['image']);
-
-//             // call api and upload image
-//             imgbbUploader("f4a18fded7b0472b695f71cbf6854d1f", obj['image'])
-//                 .then((response) => {
-//                     console.log(response);
-//                     // console.log(response.image.url);
-
-//                     // set new image url
-//                     obj['image'] = response.image.url;
-//                 })
-//                 .catch((error) => console.error(error));
-//             // break; // remove it
-//         }
-//         // console.log('after break;')
-//         // // update this file with new img urls
-//         // fs.unlinkSync(dirPath + '/' + file, (err) => {
-//         //     if (err) throw err;
-//         //     console.log(dirPath + '/' + file + ' deleted');
-//         // })
-//         // console.log('after unlink')
-//         // fs.writeFileSync(dirPath + '/' + file, JSON.stringify(data),  { flag: 'w' }, (err) => {
-//         //     if (err) throw err;
-//         //     console.log('updated ' + dirPath + '/' + file + ' written');
-//         // });
-//         // break;
-//     }
-// }
-
-// uploadImgs('./res/json0');
