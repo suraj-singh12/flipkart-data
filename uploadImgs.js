d1df935eebbbdb1dda4072a8a6ff1b6495b14504
suraj-singh12 <surajsingh5092@gmail.com>
2022-07-26 16:24:43 +0530

    working version1

--- uploadImgs.js
+++ uploadImgs.js
@@ -1,8 +1,10 @@
 const imgbbUploader = require("imgbb-uploader");
 let fs = require('fs');
 
+let dirPath = './res/json0/';
+
 // get all files
-let filesList = fs.readdirSync('./res/json0/');
+let filesList = fs.readdirSync(dirPath);
 let files = [];
 let index = 0;
 // filter (valid files only)
@@ -15,12 +17,12 @@ for (f of filesList) {
 let file = files[index];
 // console.log(file);
 
-let fileData = require('./res/json0/' + file);
+let fileData = require(dirPath + file);
 // console.log(fileData);
 
 
 doTheUploads = (fileDataIndx, image) => {
-    imgbbUploader("723d8e9efd8f7fdf5e71af62ccce564a", image)
+    imgbbUploader("12d0ff2fb51eff3efc38f953224c45b8", image)
         .then((response) => {
             // console.log(response);
             return response.url;        // send to next .then
@@ -38,15 +40,15 @@ doTheUploads = (fileDataIndx, image) => {
                 // then write this object into the file.
 
                 // // delete the old file
-                // fs.unlink('./res/json0/' + file, (err) => {
+                // fs.unlink(dirPath + file, (err) => {
                 //     if (err) throw err;
-                //     console.log('./res/json0/' + file + ' deleted');
+                //     console.log(dirPath + file + ' deleted');
                 // })
 
                 // write updated file
-                fs.writeFile('./res/json0-modified/' + file, JSON.stringify(fileData), (err) => {
+                fs.writeFile('./res/json0M/' + file, JSON.stringify(fileData), (err) => {
                     if (err) throw err;
-                    console.log('updated ' + './res/json0-modified/' + file + ' written');
+                    console.log('updated ' + './res/json0M/' + file + ' written');
                 });
 
                 // go to next file
@@ -56,7 +58,7 @@ doTheUploads = (fileDataIndx, image) => {
                 if (index == 3) {
                     if (index < files.length) {
                         file = files[index];        // now we require this file (next one)
-                        fileData = require('./res/json0/' + file);  // get this file's data
+                        fileData = require(dirPath + file);  // get this file's data
 
                         doTheUploads(0, fileData[0]['image']);
                     }
@@ -69,58 +71,3 @@ doTheUploads = (fileDataIndx, image) => {
 
 doTheUploads(0, fileData[0]['image']);
 
-
-
-
-
-// // upload all images of a file
-// const uploadImgsOf = async (file, dirPath) => {
-
-//     let fileData = require(dirPath + '/' + file);
-//     for (obj of fileData) {
-//         await imgbbUploader("723d8e9efd8f7fdf5e71af62ccce564a", obj['image'])
-//             .then((response) => {
-//                 // console.log(response);
-//                 console.log(file, obj['item_id'], response.image.url);
-//                 obj['image'] = response.image.url;
-//             })
-//             .catch((error) => console.error(error));
-//     }
-//     return fileData;
-// }
-
-// // upload images of all files in given directory & update it then
-// const uploadImgsOfDir = (dirPath) => {
-//     let files = fs.readdirSync(dirPath);
-
-//     let i = 0;
-//     // let newData = null;
-//     for (file of files) {       //  for each file
-//         if (!file.includes('json')) continue;
-//         if (file.includes('structure.json')) continue;
-//         console.log(dirPath + '/' + file);
-
-//         uploadImgsOf(file, dirPath)
-//             .then((newData) => {
-//                 // console.log(newData);
-//                 console.log('back to caller/ new data received, now writing');
-
-//                 // update this file with new img urls
-//                 fs.unlink(dirPath + '/' + file, (err) => {
-//                     if (err) throw err;
-//                     console.log(dirPath + '/' + file + ' deleted');
-//                 })
-//                 fs.writeFile(dirPath + '/' + file, JSON.stringify(newData), { flag: 'w' }, (err) => {
-//                     if (err) throw err;
-//                     console.log('updated ' + dirPath + '/' + file + ' written');
-//                 });
-//             })
-//         i++;
-//         if(i == 8) {
-//             // this is max images one account can have (near to 32 MB limit)
-//             break;
-//         }
-//     }
-// }
-
-// uploadImgsOfDir('./res/json0');
