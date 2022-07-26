74e17f5196b646a510dc95a48a14388cade1f26f
suraj-singh12 <surajsingh5092@gmail.com>
2022-07-26 14:25:57 +0530

    update patch (working)

--- uploadImgs.js
+++ uploadImgs.js
@@ -3,14 +3,18 @@ let fs = require('fs');
 let path = require('path');
 
 
-const uploadImgsOf = async (file) => {
-    for (obj of file) {
+const uploadImgsOf = async (file, dirPath) => {
+    let fileData = require(dirPath + '/' + file);
+    let i = 1;
+    for (obj of fileData) {
         await imgbbUploader("f4a18fded7b0472b695f71cbf6854d1f", obj['image'])
         .then((response) => {
             console.log(response);
             obj['image'] = response.image.url;
         })
         .catch((error) => console.error(error));
+        i++;
+        if(i == 10) break;
     }
     return fileData;
 }
@@ -18,16 +22,17 @@ const uploadImgsOf = async (file) => {
 const uploadImgsOfDir = (dirPath) => {
     let files = fs.readdirSync(dirPath);
 
+    // let newData = null;
     for (file of files) {       //  for each file
         if (!file.includes('json')) continue;
         if (file.includes('structure.json')) continue;
-
         console.log(dirPath + '/' + file);
-        let data = require(dirPath + '/' + file);
 
-        uploadImgsOf(file);
-
-        console.log('back to caller');
+        uploadImgsOf(file, dirPath)
+        .then((newData) => {
+            console.log(newData);
+            console.log('back to caller');
+        })
         break;
     }
 }
