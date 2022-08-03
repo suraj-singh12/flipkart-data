f8021bd0fd6e927b71d7ad821d03a078d278da06
suraj-singh12 <surajsingh5092@gmail.com>
2022-08-03 13:37:24 +0530

    updated price of some data

---
+++ res/json4M/increasePrices.js
@@ -0,0 +1,51 @@
+let fs = require('fs');
+const { exit } = require('process');
+let files = fs.readdirSync('./');
+
+for (file of files) {
+    if(!file.includes('json')) continue;
+    let targetLength = 5;
+    if(file.includes('camera') || file.includes('dslr') || file.includes('mobile') || file.includes('washing')) 
+        targetLength = 4;
+
+    let data = require('./' + file);
+    for(obj of data) {
+        if(!obj['old_price'] || obj['old_price'] === '') {
+            obj['old_price'] = (Number(obj['new_price']) * 1.4).toFixed(0);
+        }
+        // console.log('old price: ', obj['old_price']);
+
+        obj['old_price'] = '' + obj['old_price'];
+        obj['new_price'] = '' + obj['new_price'];
+
+        while(obj['old_price'].length < targetLength) {
+            obj['old_price'] = obj['old_price'] + '9';
+        }
+        while(obj['new_price'].length < targetLength) {
+            obj['new_price'] = obj['new_price'] + '9';
+        }
+        
+        if(obj['old_price'].length > targetLength) {
+            obj['old_price'] = obj['old_price'].substring(0, targetLength);
+        }
+        if(obj['new_price'].length > targetLength) {
+            obj['new_price'] = obj['new_price'].substring(0, targetLength);
+        }
+        
+        while(Number(obj['new_price']) > Number(obj['old_price'])) {
+            obj['old_price'] = '1' + obj['old_price'];
+        }
+        obj['old_price'] = Number(obj['old_price']);
+        obj['new_price'] = Number(obj['new_price']);
+        
+        obj['discount'] = (((obj['old_price'] - obj['new_price']) / obj['old_price']) * 100).toFixed(0);
+        
+        console.log(obj['new_price'], ' ', obj['old_price'], ' ', obj['discount']);
+        
+        // create new file
+        fs.writeFile('./modified/' + file, JSON.stringify(data), (err) => {
+            if(err) throw err;
+            console.log('updated ' + file + ' written');
+        });
+    }
+}
